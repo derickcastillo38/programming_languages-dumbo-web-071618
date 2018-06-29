e8ef08096a91890ac0db2259f89610b916ae2ad3
derickcastillo38 <9derick@gmail.com>
2018-06-29 14:31:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   languages.each do |style, lang_hash|
     lang_hash.each do |language, type_hash|
       type_hash.each do |type_symbol, type_string|
-      if new_hash[lang][:style]
+      if new_hash.key?(language)
         new_hash[lang][:style] << style unless style_arr.include?(style)
       else
         new_hash[lang] = {type: type_hash[:type], style: style_arr}
